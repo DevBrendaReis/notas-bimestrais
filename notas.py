144e6aba1795d42ac133941e4f966ad18e81cf14
DevBrendaReis <dev.brendareis@gmail.com>
2022-10-09 18:19:28 -0300

    first commit

---
+++ notas.py
@@ -0,0 +1,14 @@
+
+
+nota1 = float(input('Informe a nota do primeiro bimestre: ')) 
+nota2 = float(input('Informe a nota do segundo bimestre: ')) 
+nota3 = float(input('Informe a nota do terceiro bimestre: ')) 
+nota4 = float(input('Informe a nota do quarto bimestre: ')) 
+ 
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print('A media do aluno é: {m:.2f}'.format(m= media))
+
+
+    
+       
